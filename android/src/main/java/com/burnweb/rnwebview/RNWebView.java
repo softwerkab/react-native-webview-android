0a2c7246abe2ab74c10ef9dba24b5028acf870f9
Andriy Slyusar <andriy.slyusar@softwerk.se>
2018-08-03 17:03:31 +0200

    Fix compile error

--- android/src/main/java/com/burnweb/rnwebview/RNWebView.java
+++ android/src/main/java/com/burnweb/rnwebview/RNWebView.java
@@ -122,6 +122,35 @@ class RNWebView extends WebView implements LifecycleEventListener {
         }
     }
 
+    protected class CustomWebChromeClient extends WebChromeClient {	
+        @Override	
+        public boolean onCreateWindow(WebView view, boolean dialog, boolean userGesture, android.os.Message resultMsg) {	
+            String data = view.getHitTestResult().getExtra();	
+            if (data != null) {	
+                Uri uri = Uri.parse(data);	
+                view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, uri));	
+            } else {	
+                Log.e("RNWebView", "WebView tried to open a link in new window but did not provide URL, ignoring...");	
+            }	
+             return false;	
+        }	
+         @Override	
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {	
+            getModule().showAlert(url, message, result);	
+            return true;	
+        }	
+         // For Android 4.1+	
+        @SuppressWarnings("unused")	
+        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {	
+            getModule().startFileChooserIntent(uploadMsg, acceptType);	
+        }	
+         // For Android 5.0+	
+        @SuppressLint("NewApi")	
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {	
+            return getModule().startFileChooserIntent(filePathCallback, fileChooserParams.createIntent());	
+        }	
+    }
+
     protected class GeoWebChromeClient extends CustomWebChromeClient {
         @Override
         public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
